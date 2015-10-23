ab2c373e46dfb7a947599b185c43b4739da3cc5f
Tom Gillard <tom.gillard@reactive.com>
2015-10-23 13:27:43 +1100

    Setup strict lint options

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Dummy web app from [Let's code TDJS](http://www.letscodejavascript.com).
 - Node.JS
 - Git
 
+To build:
+1. Open command prompt
+2. Run npm install
+3. Run jake
+
--- jakefile.js
+++ jakefile.js
@@ -1,8 +1,44 @@
-task("default", ["lint"]);
+/*global desc, task, jake, fail, complete */
+(function() {
 
-desc("Lint all the things");
-task("lint", [], function() {
-	console.log("linting...");
-	var lint = require("./build/lint/lint_runner.js");
-	lint.validateFile("jakefile.js", {}, {});
-});
+	"use strict";
+
+	task("default", ["lint"]);
+
+	desc("Lint all the things");
+	task("lint", [], function() {
+
+		console.log("linting...");
+
+		var lint = require("./build/lint/lint_runner.js"),
+			files = new jake.FileList();
+			// options = require("./.jshintrc");
+
+		files.include("**/*.js");
+		files.exclude("node_modules");
+
+		lint.validateFileList(files.toArray(), nodeLintOptions(), {});
+
+	});
+
+	function nodeLintOptions() {
+		return {
+			bitwise: true,
+			curly: false,
+			eqeqeq: true,
+			forin: true,
+			immed: true,
+			latedef: true,
+			newcap: true,
+			noarg: true,
+			noempty: true,
+			nonew: true,
+			regexp: true,
+			undef: true,
+			strict: true,
+			trailing: true,
+			node: true
+		};
+	}
+
+})();
